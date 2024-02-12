88bd7a7e525d594ea6d2f3da6ad8da960f78363d
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-02-12 14:21:27 +0000

    setup picocss, htmx, alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import htmx from "astro-htmx"
+import alpinejs from "@astrojs/alpinejs";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [htmx(), alpinejs()]
 });
