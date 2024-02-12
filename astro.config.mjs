831856ced9ae3af47edc83fba85e68b95594040f
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-02-12 13:57:03 +0000

    reset development

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,9 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import markdoc from '@astrojs/markdoc'
-import keystatic from '@keystatic/astro'
-import sitemap from '@astrojs/sitemap'
-import vercel from '@astrojs/vercel/serverless'
-import Icons from 'unplugin-icons/vite'
+import { defineConfig } from 'astro/config';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.sngr.my.id',
-  integrations: [
-    react(),
-    markdoc({ allowHTML: true }),
-    keystatic(),
-    sitemap(),
-  ],
-  output: 'hybrid',
-  adapter: vercel(),
-  vite: {
-    plugins: [
-      Icons({
-        compiler: 'jsx',
-        jsx: 'react',
-      }),
-    ],
-  },
-})
+  output: "hybrid",
+  adapter: vercel()
+});
