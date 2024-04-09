cec622a8339478f8c1799382e224fc497df2d8d7
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-04-09 12:29:19 +0000

    setup home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel/serverless'
+import htmx from 'astro-htmx'
 import react from '@astrojs/react'
 import markdoc from '@astrojs/markdoc'
 import keystatic from '@keystatic/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
+  integrations: [react(), markdoc(), keystatic(), htmx()],
   output: 'hybrid',
   adapter: vercel(),
 })
