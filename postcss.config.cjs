4469ea2d6fd3585587fddcc9f7059b39d2e4a41f
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-04-14 13:54:53 +0000

    remove purgecss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,3 @@
 module.exports = {
-  plugins: [
-    require('postcss-preset-env'),
-    require('@fullhuman/postcss-purgecss')({
-      content: ['./src/**/*.astro'],
-    }),
-  ],
+  plugins: [require('postcss-preset-env')],
 }
